ba4633f24893c40e21c73871a897bbe48bd7d8e9
masaki takahashi <bje35149@gmail.com>
2024-12-24 21:22:35 +0900

    ADD：eslintの設定追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,49 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import unusedImports from "eslint-plugin-unused-imports";
 
-export default [{
+export default [
+  {
     files: ["**/*.ts"],
-}, {
+  },
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      "unused-imports": unusedImports /** 不要なimport文を削除*/,
+      "@typescript-eslint": typescriptEslint,
     },
 
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: "module",
     },
 
     rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+            "warn",
+            {
+                "vars": "all",
+                "varsIgnorePattern": "^_",
+                "args": "after-used",
+                "argsIgnorePattern": "^_",
+            },
+        ],
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          selector: "import",
+          format: ["camelCase", "PascalCase"],
+        },
+      ],
+      "@typescript-eslint/explicit-module-boundary-types": "error", /** 関数の戻り値と引数の明示的な型を追加する */
+      "@typescript-eslint/only-throw-error": "error", /** エラーのみthrowする */
+       "@typescript-eslint/no-non-null-assertion": "error", /** 非 null アサーションの禁止 */
+      curly: "warn",
+      eqeqeq: "warn",
+      "no-throw-literal": "warn",
+      semi: "warn",
     },
-}];
+  },
+];
