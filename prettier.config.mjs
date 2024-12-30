85791676f8af1efbdfd2e830cff28ecee5bc9b2b
masaki takahashi <bje35149@gmail.com>
2024-12-31 06:23:58 +0900

    ADD: prettierの設定ミス修正

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,7 +3,7 @@ const config = {
     /**  importの順序を指定する */
     "@ianvs/prettier-plugin-sort-imports",
   ],
-  trailingComma: none /** 末尾にカンマをつけない */,
+  trailingComma: "none" /** 末尾にカンマをつけない */,
   singleQuote: true,
   semi: false,
   printWidth: 100,
--- src/composable/extension.ts
+++ src/composable/extension.ts
@@ -1,16 +1,13 @@
-import * as vscode from "vscode";
+import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext): void {
-  console.log(' "codezoo" is now active!');
+  console.log(' "codezoo" is now active!')
 
-  const disposable = vscode.commands.registerCommand(
-    "codezoo.helloWorld",
-    () => {
-      vscode.window.showInformationMessage("Hello World from CodeZoo!");
-    },
-  );
+  const disposable = vscode.commands.registerCommand('codezoo.helloWorld', () => {
+    vscode.window.showInformationMessage('Hello World from CodeZoo!')
+  })
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
 
 export function deactivate(): void {}
