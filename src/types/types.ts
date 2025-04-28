e14bede65b2a816d403facb7d35733db4cd58cbc
まさきち <74603325+MASAKi-cell@users.noreply.github.com>
2025-04-29 06:01:10 +0900

    Update types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,16 @@
 /** ステータス */
 export interface CAT_STATUS {
-  stage: number // 幼少期, 成長期, 成熟期
+  stage: number // 1:幼少期, 2:成長期, 3:成熟期
   affection: number // 好感度
   energy: number // エネルギー
   satietyLevel: number // 満腹度
   isSick: boolean // 病気
   awake: boolean // 現在の時間（昼/夜）
   create_at: string // 作成日
+}
+
+export interface UI_CAT_STATUS extends CAT_STATUS {
+  stageName: string
+  affectionLevel: string
+  imageUrl: string
 }
